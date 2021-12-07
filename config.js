36fa0858eb0a11aa832d0dfd02e270a8666670f5
thcheetah777 <thcheetah777@gmail.com>
2021-12-06 17:32:56 -0800

    Crate a config

---
+++ config.js
@@ -0,0 +1,44 @@
+// Phaser config
+const config = {
+  // Type
+  type: Phaser.AUTO,
+
+  // Scaling
+  scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    mode: Phaser.Scale.RESIZE
+  },
+
+  // Rendering options
+  render: {
+    pixelArt: true
+  },
+
+  // Space
+  backgroundColor: 0x00000,
+
+  // Physics
+  physics: {
+    // Default
+    default: "arcade",
+
+    // Arcade
+    arcade: {
+      // Gravity
+      gravity: {
+        y: 1500
+      },
+
+      // Options
+      enableBody: true,
+      // debug: true
+    }
+  },
+
+  // Scenes
+  scene: {
+    preload,
+    create,
+    update
+  }
+};
