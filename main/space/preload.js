a958be4c3476581fc5cb25972fceb00cfaf9ee62
thcheetah777 <thcheetah777@gmail.com>
2021-12-09 18:38:26 -0800

    Upload space tile images

--- main/space/preload.js
+++ main/space/preload.js
@@ -1,4 +1,8 @@
 // Preload for Space
 SpaceScene.prototype.preload = function() {
-
+  // Load images
+  this.load.image("spaceTile0", "assets/spaceTile0.png");
+  this.load.image("spaceTile1", "assets/spaceTile.1png");
+  this.load.image("spaceTile2", "assets/spaceTile.2png");
+  this.load.image("spaceTile3", "assets/spaceTile.3png");
 };
