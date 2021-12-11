857726106bb765e7d2ac27937a5b32eb5d74dff6
thcheetah777 <thcheetah777@gmail.com>
2021-12-10 16:46:03 -0800

    Create space tiles

--- main/space/preload.js
+++ main/space/preload.js
@@ -1,8 +1,10 @@
 // Preload for Space
 SpaceScene.prototype.preload = function() {
   // Load images
-  this.load.image("spaceTile0", "assets/spaceTile0.png");
-  this.load.image("spaceTile1", "assets/spaceTile.1png");
-  this.load.image("spaceTile2", "assets/spaceTile.2png");
-  this.load.image("spaceTile3", "assets/spaceTile.3png");
+  this.load.image("spaceTile0", "main/space/assets/spaceTile0.png");
+  this.load.image("spaceTile1", "main/space/assets/spaceTile1.png");
+  this.load.image("spaceTile2", "main/space/assets/spaceTile2.png");
+  this.load.image("spaceTile3", "main/space/assets/spaceTile3.png");
+  this.load.image("spaceTile4", "main/space/assets/spaceTile4.png");
+  this.load.image("spaceTile5", "main/space/assets/spaceTile5.png");
 };
