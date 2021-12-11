7ef59f5e2740f52a9a643924529d31575ab503cd
thcheetah777 <thcheetah777@gmail.com>
2021-12-10 17:03:13 -0800

    Change comment

--- main/space/create.js
+++ main/space/create.js
@@ -1,6 +1,6 @@
 // Create for Space
 SpaceScene.prototype.create = function() {
-  // Background
+  // Background: Space Tiles
   for (var x = 0; x < 110; x++) {
     for (var y = 0; y < 11; y++) {
       this.add.image(x * 8 * 8, y * 8 * 8, `spaceTile${Math.round(Math.random() * 5)}`).setScale(8);
