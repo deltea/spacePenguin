03e9c4bb84cd20ddaff061d1e7ce406ba93a1051
thcheetah777 <thcheetah777@gmail.com>
2021-12-12 09:10:42 -0800

    Scrool to bottom on load

--- main/space/create.js
+++ main/space/create.js
@@ -12,7 +12,7 @@ SpaceScene.prototype.create = function() {
   }
 
   // Spaceship
-  game.spaceship = this.physics.add.sprite(500, 500, "spaceshipNormal").setGravityY(-config.physics.arcade.gravity.y).setScale(8).setDrag(30);
+  game.spaceship = this.physics.add.sprite(game.width / 2, game.height / 2, "spaceshipNormal").setGravityY(-config.physics.arcade.gravity.y).setScale(8).setDrag(30);
   game.spaceship.body.setMaxSpeed(500);
 
   // Camera
