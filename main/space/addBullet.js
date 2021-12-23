914e9f80f5e2199bccc7f7009e4b718680dcd2c4
thcheetah777 <thcheetah777@gmail.com>
2021-12-23 13:41:21 -0800

    Add a time limit to bullets

---
+++ main/space/addBullet.js
@@ -0,0 +1,16 @@
+// Function to add a bullet
+game.space.addBullet = (phaser, x, y, type) => {
+  let bullet = game.space.spaceship.bullets.create(x, y, type).setScale(8);
+  bullet.setGravityY(-config.physics.arcade.gravity.y);
+  phaser.physics.velocityFromAngle(game.space.spaceship.angle - 90, 1000, bullet.body.velocity);
+  bullet.timeLimit = phaser.time.addEvent({
+    // Time
+    delay: 600,
+
+    // Callback
+    callback: () => {
+      bullet.destroy();
+    },
+    callbackScope: this,
+  });
+}
