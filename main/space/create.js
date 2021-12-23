561596e58473ff1d1fda23ac28c422c97f2a6ca9
thcheetah777 <thcheetah777@gmail.com>
2021-12-23 12:41:30 -0800

    Add a space object for everything in space scene

--- main/space/create.js
+++ main/space/create.js
@@ -1,31 +1,31 @@
 // Create for Space
 SpaceScene.prototype.create = function() {
   // Input
-  game.cursors = this.input.keyboard.createCursorKeys();
+  game.space.cursors = this.input.keyboard.createCursorKeys();
 
   // Sound effects
-  game.sfx.normalLaser = this.sound.add("normalLaser");
-  game.sfx.cannonLaser = this.sound.add("cannonLaser");
-  game.sfx.doubleLaser = this.sound.add("doubleLaser");
-  game.sfx.explosion = this.sound.add("explosion");
-  game.sfx.hurt = this.sound.add("hurt");
-  game.sfx.teleport = this.sound.add("teleport");
-  game.sfx.bigExplosion = this.sound.add("bigExplosion");
+  game.space.sfx.normalLaser = this.sound.add("normalLaser");
+  game.space.sfx.cannonLaser = this.sound.add("cannonLaser");
+  game.space.sfx.doubleLaser = this.sound.add("doubleLaser");
+  game.space.sfx.explosion = this.sound.add("explosion");
+  game.space.sfx.hurt = this.sound.add("hurt");
+  game.space.sfx.teleport = this.sound.add("teleport");
+  game.space.sfx.bigExplosion = this.sound.add("bigExplosion");
 
   // Spaceship
-  game.spaceship = this.physics.add.sprite(game.width / 2, game.height / 2, "spaceshipNormal").setGravityY(-config.physics.arcade.gravity.y).setScale(8).setDrag(30);
-  game.spaceship.body.setMaxSpeed(500);
-  game.spaceship.bullets = this.physics.add.group();
+  game.space.spaceship = this.physics.add.sprite(game.width / 2, game.height / 2, "spaceshipNormal").setGravityY(-config.physics.arcade.gravity.y).setScale(8).setDrag(30);
+  game.space.spaceship.body.setMaxSpeed(500);
+  game.space.spaceship.bullets = this.physics.add.group();
 
   // Camera
   this.cameras.main.setBounds(0, 0, game.width, game.height);
   this.physics.world.setBounds(0, 0, game.width, game.height);
-  this.cameras.main.startFollow(game.spaceship, true, 0.1, 0.1);
+  this.cameras.main.startFollow(game.space.spaceship, true, 0.1, 0.1);
 
   // Create asteroids
-  game.asteroids = this.physics.add.group();
+  game.space.asteroids = this.physics.add.group();
   for (var i = 0; i < 100; i++) {
-    let asteroid = game.asteroids.create(Math.random() * game.width, Math.random() * game.height, "asteroid");
+    let asteroid = game.space.asteroids.create(Math.random() * game.width, Math.random() * game.height, "asteroid");
     asteroid.setScale(8);
     asteroid.setGravityY(-config.physics.arcade.gravity.y);
     asteroid.setVelocityX(Math.random() * (800 - -800) + -800);
@@ -34,14 +34,14 @@ SpaceScene.prototype.create = function() {
   }
 
   // Colliders
-  this.physics.add.collider(game.spaceship.bullets, game.asteroids, function(bullet, asteroid) {
-    game.sfx.explosion.play();
+  this.physics.add.collider(game.space.spaceship.bullets, game.space.asteroids, function(bullet, asteroid) {
+    game.space.sfx.explosion.play();
     bullet.destroy();
     asteroid.health--;
     if (asteroid.health < 1) {
-      game.sfx.bigExplosion.play();
+      game.space.sfx.bigExplosion.play();
       asteroid.destroy();
     }
   });
-  this.physics.add.collider(game.spaceship, game.asteroids);
+  this.physics.add.collider(game.space.spaceship, game.space.asteroids);
 };
